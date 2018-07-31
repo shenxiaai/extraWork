d795ccc977ef5a7066eba16747f812dfa2fee0d5
yuanbao <yuanbao@yuanbaoiMac.local>
2018-07-31 12:59:45 +0800

    audi

--- audi/aodi/js/index.js
+++ audi/aodi/js/index.js
@@ -2,13 +2,7 @@ $(
   $("#fullpage").fullpage({
     verticalCentered: false,
     css3: true,
-    //默认值：true，确定是否使用JavaScript和CSS3转换滚动在切片和幻灯片。加快平板电脑和移动设备的浏览器支持CSS3的运动有益。如果此选项设置为true，浏览器不支持CSS3，jQuery回调函数将被替代。
-    // autoScrolling:false,
-    //默认值：true，定义屏幕是否自动滚动，还是需要用户触发事件滚动，它也影响了部分适合在平板电脑和手机浏览器/设备窗口。
-    loopBottom: false,
-    //默认值：false，定义屏幕滚动到最后一个后，是否循环滚动到第一个。
-    fixedElements: '.sheader, .arrow',
-    //默认值：null，定义的某个元素是否在网页的固定位置，元素将被关闭的插件是必要的时候，使用CSS3的选项保持滚动结构固定。它需要对这些元素的jQuery选择器字符串。例如：fixedElements: ‘#element1, .element2’。
+    fixedElements: '.sheader, .arrow',//默认值：null，定义的某个元素是否在网页的固定位置，元素将被关闭的插件是必要的时候，使用CSS3的选项保持滚动结构固定。它需要对这些元素的jQuery选择器字符串。例如：fixedElements: ‘#element1, .element2’。
     afterRender: function () {
       $('.page1').find('.title').delay(500).animate({
 
